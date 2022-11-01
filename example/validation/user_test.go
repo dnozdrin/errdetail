a2e1c4198a28e067d0a381e8df303346f7d84ba1
Dmytro Nozdrin <nozdrin.dmytro@gmail.com>
2022-11-01 12:35:16 +0200

    validation example

--- example/validation/user_test.go
+++ example/validation/user_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/stretchr/testify/assert"
 
 	"github.com/dnozdrin/errdetail"
-	. "github.com/dnozdrin/errdetail/examples/validation"
+	. "github.com/dnozdrin/errdetail/example/validation"
 )
 
 func TestValidateUser(t *testing.T) {
